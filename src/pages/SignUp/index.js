72f697bdd6accb8b4998aeff998b7745298f2b26
Mahendraiqbal24 <mahendraiqbal42@gmail.com>
2022-03-31 09:38:27 +0700

    resolve validate

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,33 +1,60 @@
-import React from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState, useEffect } from "react";
 import "./SignUp.css";
 import imageVan from "../../assets/image-van.jpeg";
 import icon from "../../assets/icon.png";
 import { Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
-import {regitser} from '../../utils/https/auth';
-// import {validateSignup} from '../../helpers/validation';
-
+import { regitser } from "../../utils/https/auth";
+import { validateSignup } from "../../helpers/validation";
 
 function index(props) {
+  const [values, setValues] = useState({
+    email: "",
+    name: "",
+    password: "",
+  });
+  const [error, setError] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  // const [isLoading, setIsloading] = useState(false);
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
+    setError(validateSignup(values));
+    const validateBody = validateSignup(values);
     const body = {
       name: e.target.name.value,
       email: e.target.email.value,
       password: e.target.password.value,
     };
-
-    regitser(body)
-      .then((res) => {
-        toast.success("Registration Successfull", {
-          position: toast.POSITION.TOP_RIGHT,
-          autoClose: 2000,
-        });
-        props.history.push("/login");
-      })
-      .catch((err) => console.log(err));
+    if (Object.keys(validateBody).length === 0) {
+      setIsSubmit(true);
+      regitser(body)
+        .then((res) => {
+          toast.success("Registration Successfull", {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 2000,
+          });
+          props.history.push("/login");
+        })
+        .catch((err) => console.log(err));
+    }
   };
+
+  useEffect(() => {
+    if (Object.keys(error).length === 0 && isSubmit) {
+      console.log("submit", isSubmit);
+      console.log("useEf", error);
+    }
+  }, [error, isSubmit, props]);
   return (
     <main>
       <Row>
@@ -35,39 +62,47 @@ function index(props) {
           <img src={imageVan} className="image-van" alt="image_van"></img>
         </Col>
         <Col md={6} className="form">
-          <ToastContainer  />
+          <ToastContainer />
           <form className="submitSignUp" onSubmit={submitHandler}>
             <div className="titleSignUp">Sign Up</div>
-          <div className="input-name">
-            <input
-              type="name"
-              className="name"
-              id="name"
-              placeholder="Name"
-              name="name"
-            ></input>
-          </div>
-          <div className="input-email">
-            <input
-              type="email"
-              className="email"
-              id="email"
-              placeholder="Email"
-              name="email"
-            ></input>
-          </div>
-          <div className="input-password">
-            <input
-              type="password"
-              className="password"
-              id="password"
-              placeholder="Password"
-              name="password"
-            ></input>
-          </div>
-          <div className="button-sign-up">
-            <button className="sign-up" >Sign Up</button>
-          </div>
+            <div className="input-name">
+              <input
+                type="name"
+                className="name"
+                id="name"
+                placeholder="Name"
+                name="name"
+              ></input>
+            </div>
+            <div className="input-email">
+              <input
+                type="email"
+                className="email"
+                id="email"
+                placeholder="Email"
+                name="email"
+                onChange={handleChange}
+              ></input>
+            </div>
+            {error.email && (
+              <div className="text-danger fw-bold error">{error.email}</div>
+            )}
+            <div className="input-password">
+              <input
+                type="password"
+                className="password"
+                id="password"
+                placeholder="Password"
+                name="password"
+                onChange={handleChange}
+              ></input>
+            </div>
+            {error.password && (
+              <div className="text-danger fw-bold error">{error.password}</div>
+            )}
+            <div className="button-sign-up">
+              <button className="sign-up">Sign Up</button>
+            </div>
           </form>
           <span>try another way</span>
           <div className="button-sign-up-google">
@@ -77,13 +112,20 @@ function index(props) {
             <Link to="/login">
               <button className="signUpLogin">Login</button>
             </Link>
-          </div> <br/> <br/>
+          </div>{" "}
+          <br /> <br />
           <footer>
-              <div className="icon-signUp"><img src={icon} className="icon-rental-signup" alt="icon_rental"></img></div>
+            <div className="icon-signUp">
+              <img
+                src={icon}
+                className="icon-rental-signup"
+                alt="icon_rental"
+              ></img>
+            </div>
             <p className="text-footer-signup">
-              Plan and book your perfect trip with <br/> expert advice, travel tips
-              for vehicle <br/> information from us <br/> <br/> ©2020 Vehicle Rental Center. All
-              rights reserved
+              Plan and book your perfect trip with <br /> expert advice, travel
+              tips for vehicle <br /> information from us <br /> <br /> ©2020
+              Vehicle Rental Center. All rights reserved
             </p>
             <hr />
             <div className="icon-footer">
